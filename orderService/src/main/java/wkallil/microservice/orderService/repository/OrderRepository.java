3e1aa7ebb9eec4bd7b3b8f6ebcf78f67f1ac26cc
wkallil <kallil.tim@gmail.com>
2026-01-08 17:16:08 -0300

    refactor: change findByStatus parameter to use OrderStatus enum
    - Updates findByStatus signature in OrderRepository to accept OrderStatus instead of String

--- orderService/src/main/java/wkallil/microservice/orderService/repository/OrderRepository.java
+++ orderService/src/main/java/wkallil/microservice/orderService/repository/OrderRepository.java
@@ -2,6 +2,7 @@ package wkallil.microservice.orderService.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import wkallil.microservice.orderService.model.Order;
+import wkallil.microservice.orderService.model.OrderStatus;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,7 +11,7 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
 
     Optional<Order> findByOrderNumber(String orderNumber);
 
-    List<Order> findByStatus(String status);
+    List<Order> findByStatus(OrderStatus status);
 
     List<Order> findByCustomerName(String customerName);
 
